545ea092a560100ce739c92c868d831528d41d1b
M. Oly Mahmud <mugdhodzs38@gmail.com>
2025-05-04 22:00:51 +0600

    Create unipolar_nrz.m

---
+++ unipolar_nrz.m
@@ -0,0 +1,33 @@
+clc;
+clear all;
+close all;
+
+N = 10;
+n = randi([0,1],1,N);
+
+for m = 1:N
+    if n(m) == 1
+        nn(m) = 1;
+    else
+        nn(m) = 0;
+    end
+end
+
+n
+nn
+
+i = 1;
+t = 0:0.01:length(n);
+
+for j = 1:length(t)
+    if t(j) <= i
+        y(j) = nn(i);
+    else
+        y(j) = nn(i);
+        i = i + 1;
+    end
+end
+
+plot(t,y,'linewidth',2);
+axis([0,N,-1.5,1.5])
+grid on;
